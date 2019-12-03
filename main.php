0c67b85b19e4e9ec164106b46efc8d6a91e89d75
maciejkondev <58441559+maciejkondev@users.noreply.github.com>
2019-12-03 21:44:12 +0100

    Add files via upload

--- main.php
+++ main.php
@@ -32,7 +32,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="main2.css">
     </head>
     <body class="bg-light">
         <div>
